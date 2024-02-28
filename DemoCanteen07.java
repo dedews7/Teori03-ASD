5e4e7f359fb6bfb2bfd5453afd61e8c15124b817
Dewi Sulistiyowati <144302605+dedews7@users.noreply.github.com>
2024-02-28 19:12:54 +0700

    Add files via upload

---
+++ DemoCanteen07.java
@@ -0,0 +1,26 @@
+public class DemoCanteen07 {
+    public static void main(String[] args) {
+        item07  item1 = new item07();
+        item1.name = " Roti Bakar Om bob ";
+        item1.price = 18000;
+        item1.stock = 50;
+        item1.displayInfo();
+
+        item07  item2 = new item07();
+        item2.name = "Es Susu Om bob";
+        item2.price = 12000;
+        item2.stock = 50;
+        item2.displayInfo();
+
+        item07  item3 = new item07();
+        item3.name = " Jagung Bakar BBQ Om Bob";
+        item3.price = 10000;
+        item3.stock = 50;
+        item3.displayInfo();
+        
+
+
+
+    }
+
+}
---
+++ item07.java
@@ -0,0 +1,23 @@
+public class item07 {
+    public String name;
+    public double price;
+    public int stock;
+
+    public item07(){
+
+    }
+
+    public item07(String itemName, double itemPrice, int itemStock){
+        name = itemName;
+        price = itemPrice;
+        stock = itemStock;
+
+    }
+
+    public void displayInfo(){
+        System.out.println("Name: " + name);
+        System.out.println("Price: " + price);
+        System.out.println("Stock: " + stock);
+
+    }
+}
